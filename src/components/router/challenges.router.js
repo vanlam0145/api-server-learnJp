1f0b00972c53d75b1c282dc38ac246fb34f173b5
cross <vanlam0145@gmail.com>
2020-04-09 17:08:27 +0700

    add role admin

--- src/components/router/challenges.router.js
+++ src/components/router/challenges.router.js
@@ -1,11 +1,11 @@
 'use strict'
 const route = require('express').Router()
 const challengesCRL = require(`../controller/challenges.controller.js`)
-const userCRL = require('../controller/users.controller')
+const { authMiddleware } = require('../../helper/authMiddleware')
 const challengesRouter = function () {
     route.get('/', challengesCRL.getList)
-    route.get('/:id', userCRL.au(), challengesCRL.getById)
-    route.post('/', challengesCRL.create)
+    route.get('/:id', challengesCRL.getById)
+    route.post('/', authMiddleware(['admin']), challengesCRL.create)
     return route
 }
 module.exports = challengesRouter
