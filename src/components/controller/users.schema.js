eebb879ba6d54b8b6091cf3fd3339d747a415196
cross <vanlam0145@gmail.com>
2020-04-11 23:59:34 +0700

    use static for class

--- src/components/controller/users.schema.js
+++ src/components/controller/users.schema.js
@@ -1,12 +1,10 @@
 module.exports = class Schema {
-    constructor() {
-        this.createSchema = {
-            type: 'object',
-            properties: {
-                //name: {type: 'string'},
-            },
-            //required: ['name']
-        }
+    constructor() { }
+    static createSchema = {
+        type: 'object',
+        properties: {
+            //name: {type: 'string'},
+        },
+        //required: ['name']
     }
-
 }
