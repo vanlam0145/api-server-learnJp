c82170c8f75b838bdf3b7b5e1906dd5c2f235aeb
cross <vanlam0145@gmail.com>
2020-04-11 21:17:09 +0700

    lint challenge api

--- src/components/controller/challenges.controller.js
+++ src/components/controller/challenges.controller.js
@@ -1,34 +1,23 @@
 const ChallengesService = require('../services/challenges.services')
-const challengesSchema = require('./challenges.schema')
+const { createSchema } = require('./challenges.schema')
 const until = require('../services/untilServices')
-exports.getList = (req, res) => {
-    ChallengesService.getList()
-        .then(result => {
-            if (result) res.send(result)
-            else res.send('kaka')
-        })
-        .catch(err => {if (err) throw err})
+const { resDataModify } = require('../../helper/until')
+exports.getList = async (req, res) => {
+    const result = await ChallengesService.getList()
+    resDataModify(res, result)
 }
-exports.getById = (req, res) => {
-    ChallengesService.getById(req.params.id)
-        .then(result => {
-            if (result) res.send({
-                ...result,
-                image: getImgUrl(result.image),
-                choice_1_voice: getAudioUrl(result.choice_1_voice),
-                choice_2_voice: getAudioUrl(result.choice_2_voice)
-            })
-            else res.send('kaka')
-        })
-        .catch(err => {if (err) throw err})
-}
-exports.create = (req, res) => {
-    const vali = until.validateJson((new challengesSchema()).createSchema, req.body)
-    if (!vali.isValid) res.status(500).json({mess: vali.message})
-    else ChallengesService.create(req.body).then(result => {
-        if (result.code) res.status(result.code).json(result)
-        else res.status(201).json(result)
+exports.getById = async (req, res) => {
+    const result = await ChallengesService.getById(req.params.id)
+    resDataModify(res, {
+        ...result, image: getImgUrl(result.image),
+        choice_1_voice: getAudioUrl(result.choice_1_voice),
+        choice_2_voice: getAudioUrl(result.choice_2_voice)
     })
+}
+exports.create = async (req, res) => {
+    until.validateJson(createSchema, req.body)
+    const result = await ChallengesService.create(req.body)
+    resDataModify(res, result)
 }
 const getImgUrl = image => {
     return image
--- src/components/router/challenges.router.js
+++ src/components/router/challenges.router.js
@@ -2,8 +2,9 @@
 const route = require('express').Router()
 const challengesCRL = require(`../controller/challenges.controller.js`)
 const { authMiddleware } = require('../../helper/until')
+const { BaseRoute } = require('../../helper/baseRoute')
 const challengesRouter = function () {
-    route.get('/', challengesCRL.getList)
+    route.get('/', BaseRoute.routeModify(challengesCRL.getList))
     route.get('/:id', challengesCRL.getById)
     route.post('/', authMiddleware(['admin']), challengesCRL.create)
     return route
