ccd9945c2b655fbbf0138072ae3cfb85b4ce632a
cross <vanlam0145@gmail.com>
2020-04-11 23:58:04 +0700

    fix error

--- src/helper/errorService.js
+++ src/helper/errorService.js
@@ -1,4 +1,4 @@
-exports.error = {
+exports.ErrorService = {
     anyError(message, code) { return { message, code } },
     // Unknow
     somethingWentWrong(message) {
@@ -11,16 +11,14 @@ exports.error = {
     permissionDeny: { code: 405, status: '-3', message: 'Không đủ quyền để truy cập' },
     // Request
     requestDataInvalid(message) {
-        return { code: 403, status: '-4', message: 'Dữ liệu gửi lên không hợp lệ: ' };
+        return { code: 403, status: '-4', message: 'Dữ liệu gửi lên không hợp lệ: ' + message };
     },
     // External Request
     externalRequestFailed(message) {
         return { code: 500, status: '-5', message: 'Có lỗi xảy ra: ' };
     },
     // Mongo
-    mgRecoredNotFound() {
-        return { code: 404, status: '-7', message: 'Không tìm thấy dữ liệu yêu cầu' };
-    },
+    mgRecoredNotFound: { code: 404, status: '-7', message: 'Không tìm thấy dữ liệu yêu cầu' },
     mgQueryFailed(message) {
         return {
             code: 403, status: '-8', message: message || 'Truy vấn không thành công'
