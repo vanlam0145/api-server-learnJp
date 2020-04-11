68b739e146e9d6b7fd5a5fedd6430c4222c5d99e
cross <vanlam0145@gmail.com>
2020-04-11 23:56:24 +0700

    update function authen

--- src/helper/until.js
+++ src/helper/until.js
@@ -1,23 +1,22 @@
 const _ = require('lodash')
-const { error } = require('./errorService')
+const { ErrorService } = require('./errorService')
 const jwt = require('jsonwebtoken')
 exports.authMiddleware = (roles) =>
     (req, res, next) => {
         try {
-            let token = req.headers['x-access-token'] || req.headers.authorization.split(' ')[1]
+            let token = req.headers['x-access-token'] || (req.headers.authorization ? req.headers.authorization.split(' ')[1] : "")
             if (token) {
                 let decode = jwt.verify(token, process.env.TOKEN_SECRET)
                 req.user = decode;
-                console.log("user Login", req.user)
                 if (roles.indexOf(req.user.role) === -1)
-                    return res.status(error.badToken.code).json(error.badToken)
+                    return res.status(ErrorService.badToken.code).json(ErrorService.badToken)
                 else next();
             } else {
-                return res.status(error.unauthorized.code).json(error.unauthorized)
+                return res.status(ErrorService.unauthorized.code).json(ErrorService.unauthorized)
             }
         } catch (err) {
             if (err.name == 'TokenExpiredError')
-                return res.status(error.tokenExpired.code).json(error.tokenExpired)
+                return res.status(ErrorService.tokenExpired.code).json(ErrorService.tokenExpired)
             return res.status(500).json({ message: 'JsonWebTokenError' })
         }
     }
