2a66e8e4ca578131a4db2890aa18b1480831b571
cross <vanlam0145@gmail.com>
2020-04-11 23:57:08 +0700

    change callback to promise

--- src/helper/googleDriverApi.js
+++ src/helper/googleDriverApi.js
@@ -6,41 +6,56 @@ const path = require('path')
 const SCOPES = ['https://www.googleapis.com/auth/drive']
 const TOKEN_PATH = 'token.json';
 const errorService = require('./errorService')
-exports.authorize = function (credentials, callback) {
-    const { client_secret, client_id, redirect_uris } = credentials.installed;
-    const oAuth2Client = new google.auth.OAuth2(
-        client_id, client_secret, redirect_uris[0]);
+exports.authorize = function (credentials) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const { client_secret, client_id, redirect_uris } = credentials.installed;
+            const oAuth2Client = new google.auth.OAuth2(
+                client_id, client_secret, redirect_uris[0]);
 
-    // Check if we have previously stored a token.
-    fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) return getAccessToken(oAuth2Client, callback);
-        oAuth2Client.setCredentials(JSON.parse(token));
-        callback(oAuth2Client);
-    });
+            // Check if we have previously stored a token.
+            const token = fs.readFileSync(TOKEN_PATH)
+            oAuth2Client.setCredentials(JSON.parse(token));
+            resolve(oAuth2Client);
+        } catch (error) {
+            resolve(await getAccessToken(oAuth2Client))
+        }
+    })
+
 }
-function getAccessToken(oAuth2Client, callback) {
-    const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: SCOPES,
-    });
-    console.log('Authorize this app by visiting this url:', authUrl);
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-    rl.question('Enter the code from that page here: ', (code) => {
-        rl.close();
-        oAuth2Client.getToken(code, (err, token) => {
-            if (err) return console.error('Error retrieving access token', err);
+function getAccessToken(oAuth2Client) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const authUrl = oAuth2Client.generateAuthUrl({
+                access_type: 'offline',
+                scope: SCOPES,
+            });
+            console.log('Authorize this app by visiting this url:', authUrl);
+            const rl = readline.createInterface({
+                input: process.stdin,
+                output: process.stdout,
+            });
+            const code = new Promise((resolve2, reject2) => {
+                rl.question('Enter the code from that page here: ', (code) => {
+                    rl.close();
+                    resolve(code)
+                });
+            })
+            const token = new Promise((resolve, reject) => {
+                oAuth2Client.getToken(code, (err, token) => {
+                    if (err) reject(err);
+                    resolve(token)
+                });
+            })
             oAuth2Client.setCredentials(token);
             // Store the token to disk for later program executions
-            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-                if (err) return console.error(err);
-                console.log('Token stored to', TOKEN_PATH);
-            });
-            callback(oAuth2Client);
-        });
-    });
+            fs.writeFileSync(TOKEN_PATH, JSON.stringify(token))
+            resolve(oAuth2Client);
+        } catch (error) {
+            reject(error)
+        }
+    })
+
 }
 exports.listFiles = function (auth) {
     const drive = google.drive({ version: 'v3', auth });
@@ -91,7 +106,7 @@ exports.uploadFile = function (auth, fileimage, parents) {
 const permissionsFile = (auth, idFile) => {
     return new Promise((resolve, reject) => {
         const drive = google.drive({ version: 'v3', auth });
-        drive.permissions.create({ fileId: idFile, requestBody: { role: 'reader',type:'anyone' } }, (err, permissionFile) => {
+        drive.permissions.create({ fileId: idFile, requestBody: { role: 'reader', type: 'anyone' } }, (err, permissionFile) => {
             if (err) reject(err)
             else resolve(permissionFile)
         })
