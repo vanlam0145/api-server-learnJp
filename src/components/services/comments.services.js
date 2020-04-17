b1fb0780599ced895b0ca81b28e274f9fff713e3
cross <vanlam0145@gmail.com>
2020-04-17 23:02:57 +0700

    return comment with userName

--- src/components/services/comments.services.js
+++ src/components/services/comments.services.js
@@ -1,8 +1,13 @@
 const CommentsModel = require('../model/comments.model')
+const Types = require('mongoose').Types
 const errorService = require('../../helper/errorService')
 const untilServices = require('./untilServices')
 exports.getList = async () => await CommentsModel.find({}).exec()
-exports.getListByChallange = async (id) => await CommentsModel.find({ idChallenge: id }).exec()
+exports.getListByChallange = async (id) => await CommentsModel.aggregate([
+    { $match: { idChallenge: Types.ObjectId(id) } },
+    { $lookup: { from: "users", localField: "idUser", foreignField: "_id", as: "user" } },
+    { $unwind: "$user" },
+    { $addFields: { userName: "$user.username" } }])
 exports.getById = async (id) => await CommentsModel.findById(id).exec()
 exports.create = async function (body) {
     return await untilServices.exec(CommentsModel.create(body))
