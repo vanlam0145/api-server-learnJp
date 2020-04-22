d6e3f1578653a5b35bb2ce3ef679b4695bc4efff
cross <vanlam0145@gmail.com>
2020-04-22 16:54:12 +0700

    open Api

--- src/doc/videoLinkGg.docs.js
+++ src/doc/videoLinkGg.docs.js
@@ -6,160 +6,160 @@ exports.tag = () => {
 }
 exports.path = () => {
     return {
-        // "/videoLinkGg": {
-        //     "get": {
-        //         "tags": ["VideoLinkGg"],
-        //         "security": [
-        //             {
-        //                 "bearerAuth": []
-        //             }
-        //         ],
-        //         "summary": "get all videoLinkGg",
-        //         "responses": {
-        //             "200": {
-        //                 "description": "OK"
-        //             },
-        //             // "422": {
-        //             //   "$ref": "#/definitions/responses/UnprocessableEntity"
-        //             // },
-        //             // "401": {
-        //             //   "$ref": "#/definitions/responses/UnauthorizedError"
-        //             // }
-        //         }
-        //     },
-        //     // "post": {
-        //     //     "tags": ["VideoLinkGg"],
-        //     //     "summary": "create videoLinkGg",
-        //     //     "security": [
-        //     //         {
-        //     //             "bearerAuth": []
-        //     //         }
-        //     //     ],
-        //     //     "requestBody": {
-        //     //         "required": true,
-        //     //         "content": {
-        //     //             "application/json": {
-        //     //                 "schema": {
-        //     //                     "$ref": "#/definitions/VideoLinkGg"
-        //     //                 }
-        //     //             }
-        //     //         }
-        //     //     },
-        //     //     "responses": {
-        //     //         "200": {
-        //     //             "description": "OK",
-        //     //             "schema": {
-        //     //                 "$ref": "#/definitions/VideoLinkGg"
-        //     //             }
-        //     //         },
-        //     //         "422": {
-        //     //             "$ref": "#/definitions/responses/UnprocessableEntity"
-        //     //         }
-        //     //     }
-        //     // }
-        // },
-        // "/videoLinkGg/{id}": {
-        //     "get": {
-        //         "tags": ["VideoLinkGg"],
-        //         "security": [
-        //             {
-        //                 "bearerAuth": []
-        //             }
-        //         ],
-        //         "summary": "get details videoLinkGg",
-        //         "parameters": [
-        //             {
-        //                   "name": "id",
-        //                   "in": "path",
-        //                   "description": "id of videoLinkGg",
-        //                   "required": true,
-        //             }
-        //         ],
-        //         "responses": {
-        //             "200": {
-        //                 "description": "OK"
-        //             },
-        //             "422": {
-        //                 "$ref": "#/definitions/responses/UnprocessableEntity"
-        //             },
-        //             "401": {
-        //                 "$ref": "#/definitions/responses/UnauthorizedError"
-        //             }
-        //         }
-        //     },
-        //     // "delete": {
-        //     //     "tags": ["VideoLinkGg"],
-        //     //     "security": [
-        //     //         {
-        //     //             "bearerAuth": []
-        //     //         }
-        //     //     ],
-        //     //     "summary": "delete videoLinkGg",
-        //     //     "parameters": [
-        //     //         {
-        //     //             "name": "id",
-        //     //             "in": "path",
-        //     //             "description": "id of videoLinkGg",
-        //     //             "required": true,
-        //     //         }
-        //     //     ],
-        //     //     "responses": {
-        //     //         "200": {
-        //     //             "description": "OK",
-        //     //             "schema": {
-        //     //                 "$ref": "#/definitions/VideoLinkGg"
-        //     //             }
-        //     //         },
-        //     //         "422": {
-        //     //             "$ref": "#/definitions/responses/UnprocessableEntity"
-        //     //         },
-        //     //         "401": {
-        //     //             "$ref": "#/definitions/responses/UnauthorizedError"
-        //     //         }
-        //     //     }
-        //     // },
-        //     // "put": {
-        //     //     "tags": ["VideoLinkGg"],
-        //     //     "security": [
-        //     //         {
-        //     //             "bearerAuth": []
-        //     //         }
-        //     //     ],
-        //     //     "parameters": [
-        //     //         {
-        //     //             "name": "id",
-        //     //             "in": "path",
-        //     //             "description": "id of videoLinkGg",
-        //     //             "required": true,
-        //     //         }
-        //     //     ],
-        //     //     "summary": "update details videoLinkGg",
-        //     //     "requestBody": {
-        //     //         "required": true,
-        //     //         "content": {
-        //     //             "application/json": {
-        //     //                 "schema": {
-        //     //                     "$ref": "#/definitions/VideoLinkGg"
-        //     //                 }
-        //     //             }
-        //     //         }
-        //     //     },
-        //     //     "responses": {
-        //     //         "200": {
-        //     //             "description": "OK",
-        //     //             "schema": {
-        //     //                 "$ref": "#/definitions/VideoLinkGg"
-        //     //             }
-        //     //         },
-        //     //         "422": {
-        //     //             "$ref": "#/definitions/responses/UnprocessableEntity"
-        //     //         },
-        //     //         "401": {
-        //     //             "$ref": "#/definitions/responses/UnauthorizedError"
-        //     //         }
-        //     //     }
-        //     // },
-        // },
+        "/videoLinkGg": {
+            "get": {
+                "tags": ["VideoLinkGg"],
+                "security": [
+                    {
+                        "bearerAuth": []
+                    }
+                ],
+                "summary": "get all videoLinkGg",
+                "responses": {
+                    "200": {
+                        "description": "OK"
+                    },
+                    // "422": {
+                    //   "$ref": "#/definitions/responses/UnprocessableEntity"
+                    // },
+                    // "401": {
+                    //   "$ref": "#/definitions/responses/UnauthorizedError"
+                    // }
+                }
+            },
+            // "post": {
+            //     "tags": ["VideoLinkGg"],
+            //     "summary": "create videoLinkGg",
+            //     "security": [
+            //         {
+            //             "bearerAuth": []
+            //         }
+            //     ],
+            //     "requestBody": {
+            //         "required": true,
+            //         "content": {
+            //             "application/json": {
+            //                 "schema": {
+            //                     "$ref": "#/definitions/VideoLinkGg"
+            //                 }
+            //             }
+            //         }
+            //     },
+            //     "responses": {
+            //         "200": {
+            //             "description": "OK",
+            //             "schema": {
+            //                 "$ref": "#/definitions/VideoLinkGg"
+            //             }
+            //         },
+            //         "422": {
+            //             "$ref": "#/definitions/responses/UnprocessableEntity"
+            //         }
+            //     }
+            // }
+        },
+        "/videoLinkGg/{id}": {
+            "get": {
+                "tags": ["VideoLinkGg"],
+                "security": [
+                    {
+                        "bearerAuth": []
+                    }
+                ],
+                "summary": "get details videoLinkGg",
+                "parameters": [
+                    {
+                          "name": "id",
+                          "in": "path",
+                          "description": "id of videoLinkGg",
+                          "required": true,
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "OK"
+                    },
+                    "422": {
+                        "$ref": "#/definitions/responses/UnprocessableEntity"
+                    },
+                    "401": {
+                        "$ref": "#/definitions/responses/UnauthorizedError"
+                    }
+                }
+            },
+            // "delete": {
+            //     "tags": ["VideoLinkGg"],
+            //     "security": [
+            //         {
+            //             "bearerAuth": []
+            //         }
+            //     ],
+            //     "summary": "delete videoLinkGg",
+            //     "parameters": [
+            //         {
+            //             "name": "id",
+            //             "in": "path",
+            //             "description": "id of videoLinkGg",
+            //             "required": true,
+            //         }
+            //     ],
+            //     "responses": {
+            //         "200": {
+            //             "description": "OK",
+            //             "schema": {
+            //                 "$ref": "#/definitions/VideoLinkGg"
+            //             }
+            //         },
+            //         "422": {
+            //             "$ref": "#/definitions/responses/UnprocessableEntity"
+            //         },
+            //         "401": {
+            //             "$ref": "#/definitions/responses/UnauthorizedError"
+            //         }
+            //     }
+            // },
+            // "put": {
+            //     "tags": ["VideoLinkGg"],
+            //     "security": [
+            //         {
+            //             "bearerAuth": []
+            //         }
+            //     ],
+            //     "parameters": [
+            //         {
+            //             "name": "id",
+            //             "in": "path",
+            //             "description": "id of videoLinkGg",
+            //             "required": true,
+            //         }
+            //     ],
+            //     "summary": "update details videoLinkGg",
+            //     "requestBody": {
+            //         "required": true,
+            //         "content": {
+            //             "application/json": {
+            //                 "schema": {
+            //                     "$ref": "#/definitions/VideoLinkGg"
+            //                 }
+            //             }
+            //         }
+            //     },
+            //     "responses": {
+            //         "200": {
+            //             "description": "OK",
+            //             "schema": {
+            //                 "$ref": "#/definitions/VideoLinkGg"
+            //             }
+            //         },
+            //         "422": {
+            //             "$ref": "#/definitions/responses/UnprocessableEntity"
+            //         },
+            //         "401": {
+            //             "$ref": "#/definitions/responses/UnauthorizedError"
+            //         }
+            //     }
+            // },
+        },
     }
 }
 exports.definition = () => {
