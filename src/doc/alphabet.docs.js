1b36224f2b4ffd865e1f57033044b5eab2e19474
cross <vanlam0145@gmail.com>
2020-04-22 16:43:05 +0700

    lock api

--- src/doc/alphabet.docs.js
+++ src/doc/alphabet.docs.js
@@ -6,110 +6,110 @@ exports.tag = () => {
 }
 exports.path = () => {
     return {
-        "/alphabet": {
-            "get": {
-                "tags": ["Alphabet"],
-                "security": [
-                    {
-                        "bearerAuth": []
-                    }
-                ],
-                "summary": "get all alphabet",
-                "responses": {
-                    "200": {
-                        "description": "OK"
-                    },
-                    // "422": {
-                    //   "$ref": "#/definitions/responses/UnprocessableEntity"
-                    // },
-                    // "401": {
-                    //   "$ref": "#/definitions/responses/UnauthorizedError"
-                    // }
-                }
-            }
-        },
-        "/alphabet/{id}": {
-            "get": {
-                "tags": ["Alphabet"],
-                "security": [
-                    {
-                        "bearerAuth": []
-                    }
-                ],
-                "summary": "get details alphabet",
-                "parameters": [
-                    {
-                        "name": "id",
-                        "in": "path",
-                        "description": "id of alphabet",
-                        "required": true,
-                    }
-                ],
-                "responses": {
-                    "200": {
-                        "description": "OK"
-                    },
-                    "422": {
-                        "$ref": "#/definitions/responses/UnprocessableEntity"
-                    },
-                    "401": {
-                        "$ref": "#/definitions/responses/UnauthorizedError"
-                    }
-                }
-            },
-            "put": {
-                "tags": ["AdminDashboard"],
-                "security": [
-                    {
-                        "bearerAuth": []
-                    }
-                ],
-                "parameters": [
-                    {
-                        "name": "id",
-                        "in": "path",
-                        "description": "id of alphabet",
-                        "required": true,
-                    }
-                ],
-                "summary": "update details alphabet",
-                "requestBody": {
-                    "required": true,
-                    "content": {
-                        "application/json": {
-                            "schema": {
-                                type: 'object',
-                                properties: {
-                                    description: {
-                                        type: 'array',
-                                        items: {
-                                            type: 'object',
-                                            properties: {
-                                            }
-                                        }
-                                    }
-                                },
-                                required: ['description']
-                            }
-                        }
-                    }
-                },
-                "responses": {
-                    "200": {
-                        "description": "OK",
-                        "schema": {
-                            "$ref": "#/definitions/Alphabet"
-                        }
-                    },
-                    "422": {
-                        "$ref": "#/definitions/responses/UnprocessableEntity"
-                    },
-                    "401": {
-                        "$ref": "#/definitions/responses/UnauthorizedError"
-                    }
-                }
-            },
-        },
+        // "/alphabet": {
+        //     "get": {
+        //         "tags": ["Alphabet"],
+        //         "security": [
+        //             {
+        //                 "bearerAuth": []
+        //             }
+        //         ],
+        //         "summary": "get all alphabet",
+        //         "responses": {
+        //             "200": {
+        //                 "description": "OK"
+        //             },
+        //             // "422": {
+        //             //   "$ref": "#/definitions/responses/UnprocessableEntity"
+        //             // },
+        //             // "401": {
+        //             //   "$ref": "#/definitions/responses/UnauthorizedError"
+        //             // }
+        //         }
+        //     }
+        // },
+        // "/alphabet/{id}": {
+        //     "get": {
+        //         "tags": ["Alphabet"],
+        //         "security": [
+        //             {
+        //                 "bearerAuth": []
+        //             }
+        //         ],
+        //         "summary": "get details alphabet",
+        //         "parameters": [
+        //             {
+        //                 "name": "id",
+        //                 "in": "path",
+        //                 "description": "id of alphabet",
+        //                 "required": true,
+        //             }
+        //         ],
+        //         "responses": {
+        //             "200": {
+        //                 "description": "OK"
+        //             },
+        //             "422": {
+        //                 "$ref": "#/definitions/responses/UnprocessableEntity"
+        //             },
+        //             "401": {
+        //                 "$ref": "#/definitions/responses/UnauthorizedError"
+        //             }
+        //         }
+        //     },
+        //     "put": {
+        //         "tags": ["AdminDashboard"],
+        //         "security": [
+        //             {
+        //                 "bearerAuth": []
+        //             }
+        //         ],
+        //         "parameters": [
+        //             {
+        //                 "name": "id",
+        //                 "in": "path",
+        //                 "description": "id of alphabet",
+        //                 "required": true,
+        //             }
+        //         ],
+        //         "summary": "update details alphabet",
+        //         "requestBody": {
+        //             "required": true,
+        //             "content": {
+        //                 "application/json": {
+        //                     "schema": {
+        //                         type: 'object',
+        //                         properties: {
+        //                             description: {
+        //                                 type: 'array',
+        //                                 items: {
+        //                                     type: 'object',
+        //                                     properties: {
+        //                                     }
+        //                                 }
+        //                             }
+        //                         },
+        //                         required: ['description']
+        //                     }
+        //                 }
+        //             }
+        //         },
+        //         "responses": {
+        //             "200": {
+        //                 "description": "OK",
+        //                 "schema": {
+        //                     "$ref": "#/definitions/Alphabet"
+        //                 }
+        //             },
+        //             "422": {
+        //                 "$ref": "#/definitions/responses/UnprocessableEntity"
+        //             },
+        //             "401": {
+        //                 "$ref": "#/definitions/responses/UnauthorizedError"
+        //             }
+        //         }
+        //     },
+        // },
     }
 }
 exports.definition = () => {
