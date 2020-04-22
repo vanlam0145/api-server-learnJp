34be9673fe5fb624025032a94ef7342eebc18923
cross <vanlam0145@gmail.com>
2020-04-22 18:08:50 +0700

    alphate api and image for learn

--- src/components/model/alphabet.model.js
+++ src/components/model/alphabet.model.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const alphabetSchema = new Schema({
-	name: { type: String, required: true, unique: true },
-	singleVowel: { type: Array },
-	doubleVowel: { type: Array },
-	description: { type: Array, default: [] },
+	number: { type: Number },
+	romaji: { type: String },
+	hira: { type: String },
+	kata: { type: String },
+	example: { type: String },
 	ngTao: { type: String },
 	ngCapNhat: { type: String },
 	status: { type: String, enum: ["active", "deactive"], default: "active" }
