0e9194411181af81bbd45b25f35ec5ecbafe5394
vanlam0145 <vanlam0145@gmail.com>
2020-06-24 09:51:58 +0700

    sort index challenge

--- src/components/services/challenges.services.js
+++ src/components/services/challenges.services.js
@@ -1,14 +1,12 @@
 const ChallengesModel = require('../model/challenges.model')
 const errorService = require('../../helper/errorService')
 const untilServices = require('./untilServices')
-exports.getList = async () => await ChallengesModel.find({}).exec()
+exports.getList = async () => await ChallengesModel.find({}).sort({ index: 1 }).exec()
 exports.getById = async (id) => await ChallengesModel.findById(id).lean()
 exports.create = async function (body) {
-    const index = await ChallengesModel.count({})
     delete body.choice_1_voice
     delete body.choice_2_voice
     delete body.image
-    body.index = index
     return await untilServices.exec(ChallengesModel.create(body))
 }
 exports.addFile = async (id, type, name) => {
